b73d1d92a9b9c4726c6d685dacbfcfdb7741977c
ADKAR DNYANESH RAMDAS <dnyaneshadkar@gmail.com>
2026-01-24 21:30:42 +0530

    Many Forms

---
+++ Polymorphism/Polymorphism/MainPolymorphism.java
@@ -0,0 +1,21 @@
+package Polymorphism;
+
+public class MainPolymorphism {
+	
+	public static void main(String[] args) {
+		
+		// poly means - many
+		// morphism means - forms
+		// of the method 
+		// two types of polymorphism 
+		// compiler time polymorphism and run time polymorphism 
+		
+		Overloading ov = new Overloading();
+		ov.disp();
+		ov.disp(6);
+		
+		// we cannot achive method overloading by changing the return type of the method.
+		
+	}
+
+}
---
+++ Polymorphism/Polymorphism/Overloading.java
@@ -0,0 +1,25 @@
+package Polymorphism;
+
+public class Overloading {
+	
+	// here we will see the example of method overloading
+	 public void disp() {
+		 
+		 System.out.println("Hellow this is method overloading");
+	 }
+	 
+	 public void disp(int a) {
+		 System.out.println("This is another method of overloading ");
+	 }
+	 
+	 // method overloading means having same name of methods in the class
+	 // or in the same calss 
+	 // now the compiler will get confused which method to call 
+	 
+	 // the solution here is we can use parameters 
+	 // No of parameters 
+	 
+	 // sequence of parameters
+	 // type of parameters 
+
+}
