0150854f37ed2070699f67cb4a1c6096b1fdf1b7
ADKAR DNYANESH RAMDAS <dnyaneshadkar@gmail.com>
2026-01-29 19:16:27 +0530

    Example

---
+++ Encapsulation/encapsulation/Login.java
@@ -0,0 +1,23 @@
+package encapsulation;
+
+public class Login {
+	
+	private String myLogin;
+	private String myPassword;
+	
+	public String getMyLogin() {
+		return myLogin;
+	}
+	public void setMyLogin(String myLogin) {
+		this.myLogin = myLogin;
+	}
+	public String getMyPassword() {
+		return myPassword;
+	}
+	public void setMyPassword(String myPassword) {
+		this.myPassword = myPassword;
+	}
+	
+	
+
+}
