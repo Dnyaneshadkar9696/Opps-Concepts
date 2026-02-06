859664f28ee37b4fe0a618567afb5d1d0fd1eb31
ADKAR DNYANESH RAMDAS <dnyaneshadkar@gmail.com>
2026-02-06 22:30:37 +0530

    Single Inheritance

---
+++ inheritance/Practice Questions/Passanger.java
@@ -0,0 +1,14 @@
+package inheritance.questions;
+
+public class Passanger extends Railway{
+	
+	public void railPassangerInfo() {
+		
+		int passangerId = 2343;
+		System.out.println("Passanger information : ");
+		System.out.println("Hellow i am the indian railway traveller");
+		System.out.println("Passanger Id -"+ passangerId);
+		System.out.println("The indian railways is the big network that keeps the india running and connected");
+	}
+
+}
