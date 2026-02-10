1001da5f9280e19b0dd6d9a0e044ae13f90d13aa
ADKAR DNYANESH RAMDAS <dnyaneshadkar@gmail.com>
2026-02-10 23:42:01 +0530

    POJO class Example
    In POJO class we do 1) private members 2) public getter and setter 3) parameterized and non-parameterized constructor and 4) to String method 
    
    // The main purpose of POJO class is to store the data and to provide the data when we call the getter method and to set the data when we call the setter method

---
+++ Encapsulation/encapsulation/Practice/practise/Student.java
@@ -0,0 +1,65 @@
+package encapsulation.practise;
+
+public class Student {
+	
+	// example of a pojo class 
+	
+
+	
+//	    Pojo class format is as follows
+//	    1. declare the variables as private
+	//	    2. generate the public getter and setter method
+	//	    3. use the toString method
+	// write the parameterized and non parameterized constructor
+	
+	
+	private String name;
+	private int rollNo;
+	private String city;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getRollNo() {
+		return rollNo;
+	}
+	
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", rollNo=" + rollNo + ", city=" + city + "]";
+	}
+		
+	public Student() {
+		
+		
+	}
+	
+	public Student(String name, int rollNo, String city) {
+	
+		this.name = name;
+		this.rollNo = rollNo;
+		this.city = city;
+	}
+	
+	
+	
+}
+
+
