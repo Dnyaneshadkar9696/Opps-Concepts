c96aaf6332170b393cbe7d9d1aef4146a5d9bcb4
ADKAR DNYANESH RAMDAS <dnyaneshadkar@gmail.com>
2026-02-12 07:57:12 +0530

    Expand examples in Calling.java for method overriding
    Added additional examples demonstrating method overriding functionality.

--- Polymorphism/Polymorphism/ClassPolymorphism/Overriding/Calling.java
+++ Polymorphism/Polymorphism/ClassPolymorphism/Overriding/Calling.java
@@ -38,8 +38,92 @@ public class Calling {
 		System.out.println();
 		NameSame nm = new NameSame();
 		nm.nameInfo(4);
+
+		// Example 07
+		System.out.println();
+		AccessM acm = new AccessM();
+		acm.accssInfo();
+		
+		//Example 08
+		System.out.println();
+		MarkF mf = new MarkF();
+		mf.markInfo();
+		
+		//Example 09
+		System.out.println();
+	   Son sn = new Son();
+	   sn.grandFather();
+	   
+	   // Example 10
+	   System.out.println();
+	   Icici ic = new Icici();
+	   ic.interestRates();
+	   
+	   //Example 11
+	   System.out.println();
+	   Manager m = new Manager();
+	   m.salary();
+	   Developer dev = new Developer();
+	   dev.salary();
+	   
+	   //Example 12
+	   System.out.println();
+	   UPI up = new UPI();
+	   up.pay();
+	   CreditCard cc = new CreditCard();
+	   cc.pay();
+	   
+//	   Example 13
+	   System.out.println();
+	   Pizza pi = new Pizza();
+	   pi.taste();
+	   Burger bi = new Burger();
+	   bi.taste();
+	   
+	   //Example 14
+	   System.out.println();
+	   Payment py = new Payment();
+	   py.password();
+	   
+	   //Example 15
+	   System.out.println();
+	   Rooms rm = new Rooms();
+	   rm.houseInfo();
+	   
+	   // Example 16
+	   System.out.println();
+	   TPlink tp  = new TPlink();
+	   tp.speed();
+	   
+	   // Example 17
+	   System.out.println();
+	   Odd od = new Odd();
+	   od.checkEven(8);
+	   
+	   //Example 18
+	   System.out.println();
+	   HospitalDetails hd = new HospitalDetails();
+	   hd.patient();
+	   hd.doctor();
+	   hd.nurse();
+	   
+	   
+	   //Example 19
+	   System.out.println();
+	   LibDetails ld = new LibDetails();
+	   ld.newspaper();
+	   ld.magazine();
+	   ld.book();
+	   
+	   // Example 20
+	   System.out.println();
+	   ShopDetails sd = new ShopDetails();
+	   sd.product();
+	   sd.quantity();
+	   sd.price();
 		
 		
 	}
 
 }
+
